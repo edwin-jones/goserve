f1862ace44e63249a1e3ada010c58f4eaeddd962
Edwin Jones <edwin.jones@mediatonic.co.uk>
2019-07-24 14:43:29 +0100

    create very basic webserver with one response.

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	host     = "localhost"
+	port     = "8080"
+	protocol = "tcp"
+)
+
 func main() {
-    fmt.Println("hello world")
+	// Listen for incoming connections.
+	l, err := net.Listen(protocol, host+":"+port)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	// Close the listener when the application closes.
+	defer l.Close()
+
+	fmt.Println("Listening on " + host + ":" + port)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	// Send a response back to person contacting us.
+	conn.Write([]byte("HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!"))
+	// Close the connection when you're done with it.
+	conn.Close()
 }
