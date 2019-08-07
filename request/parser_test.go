93825c8608a5b5fc055f479ba8c7fad48734f152
Edwin Jones <edwin.jones@mediatonic.co.uk>
2019-08-07 17:37:21 +0100

    Update tests

--- request/parser_test.go
+++ request/parser_test.go
@@ -2,44 +2,69 @@ package request
 
 import (
 	"testing"
+
+	"github.com/dchest/uniuri"
+	"github.com/edwin-jones/goserve/status"
 )
 
-func TestBlankRequestValidation(t *testing.T) {
-	if _, err := Parse(""); err == nil {
-		t.Error("expected error response")
-	} else {
-		if err.Response != badRequest {
-			t.Error("expected 400 bad request error response")
-		}
-	}
+type testFileChecker struct {
+	exists bool
 }
 
-func TestIncompleteRequestValidation(t *testing.T) {
-	if _, err := Parse("GET foo"); err == nil {
-		t.Error("expected error response")
-	} else {
-		if err.Response != badRequest {
-			t.Error("expected 400 bad request error response")
-		}
-	}
+func (t testFileChecker) Exists(path string) bool {
+	return t.exists
 }
 
-func TestInvalidMethodValidation(t *testing.T) {
-	if _, err := Parse("DELETE"); err == nil {
-		t.Error("expected error response")
-	} else {
-		if err.Response != invalidHTTPMethod {
-			t.Error("expected 405 invalid method error response")
-		}
+func TestStatusValidation(t *testing.T) {
+	testCases := []struct {
+		request            string
+		expectedStatusCode status.Code
+		fileExists         bool
+	}{
+		{
+			request:            "",
+			expectedStatusCode: status.BadRequest,
+			fileExists:         true,
+		},
+		{
+			request:            "GET foo",
+			expectedStatusCode: status.BadRequest,
+			fileExists:         true,
+		},
+		{
+			request:            "GET test.foo",
+			expectedStatusCode: status.UnsupportedMediaType,
+			fileExists:         true,
+		},
+		{
+			request:            "GET test.html" + uniuri.NewLen(1000),
+			expectedStatusCode: status.URITooLong,
+			fileExists:         true,
+		},
+		{
+			request:            "DELETE",
+			expectedStatusCode: status.InvalidHTTPMethod,
+			fileExists:         true,
+		},
+		{
+			request:            "GET test.html",
+			expectedStatusCode: status.Success,
+			fileExists:         true,
+		},
+		{
+			request:            "GET test.html",
+			expectedStatusCode: status.NotFound,
+			fileExists:         false,
+		},
 	}
-}
 
-func TestUnsupportedFiletypeValidation(t *testing.T) {
-	if _, err := Parse("GET http://localhost:8080/test.foo"); err == nil {
-		t.Error("expected error response")
-	} else {
-		if err.Response != unsupportedMediaType {
-			t.Error("expected 415 unsupported filetype error response")
+	for _, c := range testCases {
+		fileChecker := testFileChecker{exists: c.fileExists}
+		parser := NewParser(fileChecker)
+
+		_, code := parser.Parse([]byte(c.request))
+		if code != c.expectedStatusCode {
+			t.Errorf("Expected status code %d, got %d", c.expectedStatusCode, code)
 		}
 	}
 }
